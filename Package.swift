48663c94a39b41463770d5998706ec60407b074d
Amaan Qureshi <amaanq12@gmail.com>
2024-10-14 17:06:27 -0400

    build: update bindings and migrate to 0.24

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "TreeSitterAgda",
+    products: [
+        .library(name: "TreeSitterAgda", targets: ["TreeSitterAgda"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ChimeHQ/SwiftTreeSitter", from: "0.8.0"),
+    ],
+    targets: [
+        .target(
+            name: "TreeSitterAgda",
+            dependencies: [],
+            path: ".",
+            sources: [
+                "src/parser.c",
+                "src/scanner.c",
+            ],
+            resources: [
+                .copy("queries")
+            ],
+            publicHeadersPath: "bindings/swift",
+            cSettings: [.headerSearchPath("src")]
+        ),
+        .testTarget(
+            name: "TreeSitterAgdaTests",
+            dependencies: [
+                "SwiftTreeSitter",
+                "TreeSitterAgda",
+            ],
+            path: "bindings/swift/TreeSitterAgdaTests"
+        )
+    ],
+    cLanguageStandard: .c11
+)
---
+++ bindings/python/tree_sitter_agda/__init__.pyi
@@ -0,0 +1,5 @@
+from typing import Final
+
+HIGHLIGHTS_QUERY: Final[str]
+
+def language() -> object: ...
